ac15c00ae2c86128e5db5f5c5b6faa18c6965bc3
alvaro5monge <Alvarom201999@gmail.com>
2020-03-05 19:03:07 +0100

    fds

--- backend/Baseball/src/main/java/com/baseball/Baseball/Players/Controller.java
+++ backend/Baseball/src/main/java/com/baseball/Baseball/Players/Controller.java
@@ -1,16 +1,26 @@
 package com.baseball.Baseball.Players;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
 @RestController
 @RequestMapping("/player")
 public class Controller {
+
+    @Autowired
     private PlayerRepository repository;
-    @GetMapping("/{id}")
-    Mono<Player> getById(@PathVariable(value = "id") String id){
-        return this.repository.findById(id);
+
+    @GetMapping("{id}")
+    Mono<ResponseEntity<Player>> getById(@PathVariable(value = "id") String id){
+        return this.repository.findById(id)
+                .map(player -> ResponseEntity.ok(player))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+
+
     }
 
     @PostMapping()
--- backend/Baseball/src/main/java/com/baseball/Baseball/Players/PageableController.java
+++ backend/Baseball/src/main/java/com/baseball/Baseball/Players/PageableController.java
@@ -3,7 +3,9 @@ package com.baseball.Baseball.Players;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -13,8 +15,11 @@ public class PageableController {
     @Autowired
     private PageableRepository repository;
     @GetMapping()
-    private Mono<Page<Player>> getAllPlayers(@RequestParam(name = "page", defaultValue = "0") Integer page,
+    private Page<Player> getAllPlayers(@RequestParam(name = "page", defaultValue = "0") Integer page,
                                              @RequestParam(name = "size", defaultValue = "10") Integer size){
-        return Mono.just(this.repository.findAll(PageRequest.of(page,size)));
+        System.out.println("Hola");
+        return this.repository.findAll(PageRequest.of(page,size));
+        //.map( fvfv -> ResponseEntity.ok());
+        // .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
